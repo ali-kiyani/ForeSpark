a7d9e83f3d7875ba3883e144e1d3711c7a804f14
ali-kiyani <malikiyani4@gmail.com>
2020-12-07 02:27:31 +0500

    update CORS

--- aspnet-core/src/ForeSpark.Web.Host/Startup/Startup.cs
+++ aspnet-core/src/ForeSpark.Web.Host/Startup/Startup.cs
@@ -65,17 +65,11 @@ namespace ForeSpark.Web.Host.Startup
                 options => options.AddPolicy(
                     _defaultCorsPolicyName,
                     builder => builder
-                    .WithOrigins(
-                            // App:CorsOrigins in appsettings.json can contain more than one address separated by comma.
-                            _appConfiguration["App:CorsOrigins"]
-                                .Split(",", StringSplitOptions.RemoveEmptyEntries)
-                                .Select(o => o.RemovePostFix("/"))
-                                .ToArray()
-                        )
-                        //.AllowAnyOrigin()
+                    
+                        .AllowAnyOrigin()
                         .AllowAnyHeader()
                         .AllowAnyMethod()
-                        .AllowCredentials()
+                        //.AllowCredentials()
                 )
             );
 
